8843b2f64fc17e843452c8d7105bc04805a9cefd
gwonyejun <kyjuni1059@gmail.com>
2023-06-18 16:48:23 +0900

    메인 페이지 로컬 서버를 만들어 띄워 주었다.

--- main/src/controller/servser.ts
+++ main/src/controller/servser.ts
@@ -1,1 +1,17 @@
-import express from 'express'
+import express,{Response,Request} from 'express'
+import path, { dirname } from 'path'
+const app = express();
+
+const root = path.join(__dirname, "../../")
+console.log(root)
+
+app.use(express.static(path.join(root, 'build')))
+
+app.get("*", (req: Request, res: Response) => {
+  const filePath = path.join(root,'index.html');
+  res.sendFile(filePath);
+});
+
+app.listen(1111, () => {
+  console.log('서버를 열었습니다.');
+})
