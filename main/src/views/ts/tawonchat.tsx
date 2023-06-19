96082c36ce9b99c78465a61645ed2fee7bfc85b6
gwonyejun <kyjuni1059@gmail.com>
2023-06-19 22:40:01 +0900

    차트 수정

--- main/src/views/ts/tawonchat.tsx
+++ main/src/views/ts/tawonchat.tsx
@@ -2,51 +2,47 @@ import { ResponsiveRadar } from '@nivo/radar'
 import React from 'react';
 
 const Tawonechat = () => {
- const data = [
+ const data= [
     {
-      "taste": "fruity",
-      "chardonay": 117,
-      "carmenere": 68,
-      "syrah": 91
+      "taste": "정서안정성",
+      "정성검사": 82,
     },
     {
-      "taste": "bitter",
-      "chardonay": 74,
-      "carmenere": 118,
-      "syrah": 24
+      "taste": "개방성",
+      "정성검사": 73,
     },
     {
-      "taste": "heavy",
-      "chardonay": 88,
-      "carmenere": 56,
-      "syrah": 46
+      "taste": "성실성",
+      "정성검사": 71,
     },
     {
-      "taste": "strong",
-      "chardonay": 113,
-      "carmenere": 99,
-      "syrah": 45
+      "taste": "원만성",
+      "정성검사": 64,
     },
     {
-      "taste": "sunny",
-      "chardonay": 119,
-      "carmenere": 107,
-      "syrah": 107
+      "taste": "외향성",
+      "정성검사": 58,
+    }
+    ,
+    {
+      "taste": "정직겸손성",
+      "정성검사": 63,
     }
   ]
   return (
     <ResponsiveRadar
         data={data}
-        keys={[ 'chardonay', 'carmenere', 'syrah' ]}
+        keys={[ '정성검사']}
         indexBy="taste"
         valueFormat=">-.2f"
-        margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+        margin={{ top: 70, right: 80, bottom: 45, left: 80 }}
         borderColor={{ from: 'color' }}
-        gridLabelOffset={36}
+        gridLabelOffset={20}
         dotSize={10}
         dotColor={{ theme: 'background' }}
-        dotBorderWidth={2}
-        colors={{ scheme: 'nivo' }}
+        dotBorderWidth={3}
+        colors={{ scheme: 'set3' }}
+        fillOpacity={0.2}
         blendMode="multiply"
         motionConfig="wobbly"
         legends={[
@@ -57,14 +53,14 @@ const Tawonechat = () => {
                 translateY: -40,
                 itemWidth: 80,
                 itemHeight: 20,
-                itemTextColor: '#999',
+                itemTextColor: '#3B56D3',
                 symbolSize: 12,
                 symbolShape: 'circle',
                 effects: [
                     {
                         on: 'hover',
                         style: {
-                            itemTextColor: '#000'
+                            itemTextColor: '#3B56D3'
                         }
                     }
                 ]
