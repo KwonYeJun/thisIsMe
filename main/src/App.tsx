49f0a865cb3508dbc3d69e68105c4a27714a7f4b
gwonyejun <kyjuni1059@gmail.com>
2023-06-19 23:15:51 +0900

    다크 모드 버튼 생성

--- main/src/App.tsx
+++ main/src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Name from './views/ts/name'
 import Tawonchat from './views/ts/tawonchat'
 function App() {
+  const [darkMode, setDarkMode] = useState<any>(false);
 
+  // 페이지가 로드될 때 다크 모드 설정을 확인합니다.
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+    setDarkMode(isDarkMode);
+  }, []);
+
+  // 다크 모드 설정 변경 시 로컬 스토리지에 반영합니다.
+  useEffect(() => {
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  // 다크 모드 상태를 토글합니다.
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
     <>
-      <div id="container">
+      <div className={`${darkMode ? 'dark-mode' : ''}`}>
+        <div id="container">
 
-        <div>
-          <Name />
-          <Tawonchat />
+          <div>
+            <h1>Welcome to My Website</h1>
+            <p>This is some content on my website.</p>
+            <button onClick={toggleDarkMode}>
+              {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
+
+          </div>
+          <div>  <Name />
+            <Tawonchat /></div>
+          <div>누가바
+            누 : 누가바
+            가 : 가까이서보니
+            바 : 바밤바</div>
         </div>
-        <div>바밤바
-          바 : 바밤바
-          밤 : 밤맛나는
-          바 : 바밤바</div>
-        <div>누가바
-          누 : 누가바
-          가 : 가까이서보니
-          바 : 바밤바</div>
       </div>
     </>
   );
