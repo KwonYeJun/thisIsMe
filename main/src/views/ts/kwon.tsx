95a1d868e798583299aeaa77f35a31719b81b6ff
gwonyejun <kyjuni1059@gmail.com>
2023-06-24 18:22:31 +0900

    이미지 추가 및 수정 중

--- main/src/views/ts/kwon.tsx
+++ main/src/views/ts/kwon.tsx
@@ -1,15 +1,26 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AnimatePresence, motion } from 'framer-motion';
 
 
 export default function Kwon() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = ['../img/navy.jpg', '../img/green.jpg', '../img/pink.jpg'];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
 
 
   return (
-    <div className="backGray">
-      asdfasdfsadf
-     test1
+    <div className="kwonpage">
+      <div className="textdiv">asd</div>
+      <div className="imgdiv"> <img src={images[currentImage]} /></div>
+
     </div>
   );
 };
